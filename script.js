73fee65b3c133ced8226c772863bf332e2340a32
shaima2322 <109865711+shaima2322@users.noreply.github.com>
2022-08-19 20:13:08 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function BMI(weight, height) {
+let result = weight / (height * height);
+return result;
+}
+console.log(BMI(50,1.66) );
 
+function status(bmi) {
+
+if (bmi < 18.5) {
+    return "لديك نقص في الوزن";
+} else if (bmi >=18.5 && bmi< 25) {
+    return "وزنك صحي";
+} else {
+    return "لديك زياده في الوزن";
+}
+}
+function calculate() {
+
+    let weight = document.getElementById(weight).value;
+    let height = document.getElementById(height).value;
+
+let bmi = BMI(weight ,height);
+
+let desc = satuts(bmi);
+
+let div = document.getElementById;("result");
+div.innerText = bmi + " == " + desc;
+}
